3cfadeca6ef3f151b2a7f4c706febb010ae9f4bd
𝐉𝐅𝐋𝐄𝐗 <162643233+marry2001@users.noreply.github.com>
2024-04-02 14:47:21 +0300

    Update Retrieve.ts

--- src/Commands/Utils/Retrieve.ts
+++ src/Commands/Utils/Retrieve.ts
@@ -4,15 +4,15 @@ import { BaseCommand, Command, Message } from '../../Structures'
     description: 'Retrieves view once message',
     category: 'utils',
     usage: 'retrieve [quote view once message]',
-    cooldown: 5,
+    cooldown: 10,
     exp: 40
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message): Promise<void> => {
-        if (!M.quoted || M.quoted.type !== 'viewOnceMessage')
+        if (!M.quoted || M.quoted.type !== 'viewOnceMessageV2')
             return void M.reply('Quote a view once message to retrieve, Baka!')
         const buffer = await M.downloadMediaMessage(M.quoted.message)
-        const type = Object.keys(M.quoted.message.viewOnceMessage?.message || {})[0].replace('Message', '') as
+        const type = Object.keys(M.quoted.message.viewOnceMessageV2?.message || {})[0].replace('Message', '') as
             | 'image'
             | 'video'
         return void (await M.reply(buffer, type))
